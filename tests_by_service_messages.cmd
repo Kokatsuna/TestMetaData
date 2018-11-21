60e440bd94c01fbf8da34a3a193c2c1de734a602
Julia Reshetnikova <julia.reshetnikova@jetbrains.com>
2018-11-21 15:57:17 +0300

    fix quotes 2

--- tests_by_service_messages.cmd
+++ tests_by_service_messages.cmd
@@ -1,4 +1,4 @@
-echo "##teamcity[testSuiteStarted name='My Suite']
+echo "##teamcity[testSuiteStarted name='My Suite']"
 echo "##teamcity[testStarted name='testName']"
 echo "##teamcity[publishArtifacts 'data/screenshot1.png']"
 echo "##teamcity[testMetadata testName='testName' key='a screenshot' type='image' value='data/screenshot1.png']"
