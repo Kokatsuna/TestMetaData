efdc26b54a26615db496c4435f1d12ab5ae02f13
Julia Reshetnikova <julia.reshetnikova@jetbrains.com>
2018-11-21 15:45:42 +0300

    add screenshot and initial cmd script

---
+++ tests_by_service_messages.cmd
@@ -0,0 +1,6 @@
+echo "##teamcity[testSuiteStarted name='My Suite']
+echo "##teamcity[testStarted name='testName']"
+echo "##teamcity[publishArtifacts 'data/screenshot1.png']"
+echo "##teamcity[testMetadata testName='testName' key='a screenshot' type='image' value='data/screenshot1.png']
+echo "##teamcity[testFinished name='testName']"
+echo "##teamcity[testSuiteFinished name='My Suite']
