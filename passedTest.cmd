9e5efcf7fcb349a1d55b6552f535d8bc10c91501
Repository with test projects <teamcity.qa.server@gmail.com>
2018-11-23 13:58:46 +0300

    Update passedTest.cmd

--- passedTest.cmd
+++ passedTest.cmd
@@ -1,6 +1,4 @@
 echo "##teamcity[testSuiteStarted name='My Suite']"
-echo "##teamcity[testStarted name='passedTest']"
-echo "##teamcity[testFinished name='passedTest']"
 echo "##teamcity[testStarted name='twoAttachments']"
 echo "##teamcity[publishArtifacts 'data/screenshot1.png => data']"
 echo "##teamcity[publishArtifacts 'data/purr.gif => data']"
