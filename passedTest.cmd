016da699e4283ef74dddf017a1fcad8bc0fea650
Julia Reshetnikova <julia.reshetnikova@jetbrains.com>
2018-11-23 13:40:20 +0300

    add gif, change name to testName, add type

---
+++ passedTest.cmd
@@ -0,0 +1,8 @@
+echo "##teamcity[testSuiteStarted name='My Suite']"
+echo "##teamcity[testStarted name='passedTest']"
+echo "##teamcity[publishArtifacts 'data/screenshot1.png => data']"
+echo "##teamcity[publishArtifacts 'data/purr.gif => data']"
+echo "##teamcity[testMetadata testName='passedTest' key='a screenshot' type='image' value='data/screenshot1.png']"
+echo "##teamcity[testMetadata testName='passedTest' key='a gif' type='image' value='data/purr.gif']"
+echo "##teamcity[testFinished testName='passedTest']"
+echo "##teamcity[testSuiteFinished name='My Suite']"
