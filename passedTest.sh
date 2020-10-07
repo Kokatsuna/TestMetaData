8057f106e7159892c58934eb601b7b12c29978cf
Vladimir Ivanov <kokatsuna@yandex.ru>
2020-10-07 23:17:39 +0300

    Update passedTest.sh

--- passedTest.sh
+++ passedTest.sh
@@ -1,7 +1,7 @@
 echo "##teamcity[testSuiteStarted name='My Suite']"
 echo "##teamcity[testStarted name='twoAttachments']"
 echo "##teamcity[publishArtifacts 'data/screenshot1.png => data']"
-echo "##teamcity[publishArtifacts 'data/purr.gif => data']"
+# echo "##teamcity[publishArtifacts 'data/purr.gif => data']"
 echo "##teamcity[testMetadata testName='twoAttachments' key='a screenshot' type='image' value='data/screenshot1.png']"
 echo "##teamcity[testMetadata testName='twoAttachments' key='a gif' type='image' value='data/purr.gif']"
 echo "##teamcity[testFinished name='twoAttachments']"
