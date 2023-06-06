323e745fe94b80fcceade40c40fd8a4bdcbed889
Vladimir Ivanov <vladimir.ivanov@jetbrains.com>
2023-06-06 14:13:52 +0400

    Qodana test

---
+++ qodana-test/src/test.js
@@ -0,0 +1,15 @@
+const mergeOptions = require('merge-options');
+
+const baseConfig = require('./karma-base.conf');
+
+module.exports = config => {
+    const configWatch = mergeOptions(baseConfig(config), {
+        singleRun: false,
+        reporters: ['progress', 'osx'],
+        osxReporter: {
+            notificationMode: 'change'
+        }
+    });
+
+    config.set(configWatch);
+};
